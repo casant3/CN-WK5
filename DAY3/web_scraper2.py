1e2d4223bedf5c69ae40a6affcf090a709081ff2
Cassey <casante3103@gmail.com>
2024-03-01 15:39:31 +0000

    deleted files

--- DAY3/web_scraper2.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-
-html_text = requests.get("http://127.0.0.1:5500/test2.html")
-
-souped_html = BeautifulSoup(html_text.text,"lxml")
-
-countries = souped_html.find_all("h3")
